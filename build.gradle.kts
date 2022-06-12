bc736f045128d82985d43b03e855a59fb0d3840e
Sulaiman Oboody <lsafer@cufy.org>
2022-06-12 03:30:15 +0300

    build: Updated dependencies
    - updated to kotlin 1.7.0
    - added kotlinx serialization
    - added mongo driver dependencies
    - added litote kmongo driver dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.serialization") version "1.7.0"
     id("maven-publish")
 }
 
@@ -14,7 +15,13 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+
+    implementation("org.litote.kmongo:kmongo:4.6.0")
+    implementation("org.litote.kmongo:kmongo-coroutine:4.6.0")
+    implementation("org.mongodb:mongodb-driver-sync:4.6.0")
+    implementation("org.mongodb:mongodb-driver-reactivestreams:4.6.0")
 
     testImplementation(kotlin("test"))
 }
