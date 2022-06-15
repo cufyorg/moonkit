acc570d88de2d271a9bf6ec801c17a4501e91d69
Sulaiman Oboody <lsafer@cufy.org>
2022-06-15 20:56:19 +0300

    feat: Added schema extension `insure`

---
+++ src/main/kotlin/extension/insure.kt
@@ -0,0 +1,36 @@
+package org.cufy.mangaka.extension
+
+import org.cufy.mangaka.Schema
+import org.cufy.mangaka.SchemaScope
+
+/**
+ * Add the given validator [function] to this
+ * schema and apply it only when the value is
+ * present.
+ *
+ * @since 1.0.0
+ */
+fun <D, O, T> Schema<D, O, T>.insure(
+    message: String,
+    function: SchemaScope<D, O, T>.(T) -> Boolean
+) {
+    insure({ message }, function)
+}
+
+/**
+ * Add the given validator [function] to this
+ * schema and apply it only when the value is
+ * present.
+ *
+ * @since 1.0.0
+ */
+fun <D, O, T> Schema<D, O, T>.insure(
+    message: SchemaScope<D, O, T>.(T) -> String = {
+        "Validation failed for path $path"
+    },
+    function: suspend SchemaScope<D, O, T>.(T) -> Boolean
+) {
+    validate({ message(it!!) }) {
+        it == null || function(it)
+    }
+}
