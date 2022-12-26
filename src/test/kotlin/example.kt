4b77b702af53e6171835ea6827a5406882dfe5f9
Sulaiman Oboody <lsafer@cufy.org>
2022-12-26 16:36:43 +0300

    test: Updated tests

--- src/test/kotlin/example.kt
+++ src/test/kotlin/example.kt
@@ -6,10 +6,7 @@ import org.cufy.bson.bstring
 import org.cufy.monkt.*
 import org.cufy.monkt.schema.*
 import org.cufy.monkt.schema.extension.*
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.*
 import java.util.*
 import java.util.UUID.randomUUID
 import kotlin.test.assertEquals
@@ -31,7 +28,7 @@ class ExampleTest {
     @AfterEach
     fun cleanup() {
         runBlocking {
-            monkt.database.dropSuspend()
+            monkt.getDatabase().dropSuspend()
             monkt.shutdown()
         }
     }
@@ -64,7 +61,7 @@ class ExampleTest {
 
             instance.save()
 
-            val source = Model1.collection.findSuspend().single()
+            val source = Model1.getCollection().findSuspend().single()
 
             assertEquals(setOf("_id"), source.keys)
         }
@@ -103,7 +100,7 @@ class ExampleTest {
 
             instance.save()
 
-            val source = Model1.collection.findSuspend().single()
+            val source = Model1.getCollection().findSuspend().single()
 
             assertEquals(setOf("_id", "value"), source.keys)
             assertEquals(source["value"], bnull)
@@ -134,7 +131,7 @@ class ExampleTest {
 
             instance.save()
 
-            val source0 = Model1.collection.findOneSuspend({ "_id" by instance._id })!!
+            val source0 = Model1.getCollection().findOneSuspend({ "_id" by instance._id })!!
 
             assertEquals(bstring("Alpha"), source0["value"])
 
@@ -142,9 +139,86 @@ class ExampleTest {
 
             instance.save()
 
-            val source1 = Model1.collection.findOneSuspend({ "_id" by instance._id })!!
+            val source1 = Model1.getCollection().findOneSuspend({ "_id" by instance._id })!!
 
             assertEquals(bstring("Alpha"), source1["value"])
+        }
+    }
+
+    @Test
+    fun `options - multiple levels of dependency and mapping`() {
+        class Document1 : Document {
+            val value: String by Document.fantom()
+        }
+
+        class Document2 : Document {
+            val name: String by Document.fantom()
+            val alt: String? by Document.fantom()
+            var dependencies: List<Document1> by Document.fantom()
+        }
+
+        val Schema1 = ObjectSchema(::Document1) {
+            fantom("name", StringSchema)
+            fantom("value", StringSchema)
+        }
+
+        val Model1 = Model("Document1", Schema1)
+
+        val Model2 = Model("Document2", ::Document2) {
+            fantom("name", StringSchema)
+            fantom("alt", NullableSchema(StringSchema)) {
+                default { null }
+            }
+            fantom("dependencies", ArraySchema(Schema1)) {
+                init {
+                    val count by count(Model1) { "name" by instance.name }
+                    val instances by find(Model1) { "name" by instance.name }
+
+                    assertThrows<IllegalStateException> { println(count) }
+                    assertThrows<IllegalStateException> { println(instances) }
+
+                    wait()
+
+                    assertEquals(count, instances.size.toLong())
+
+                    instance.dependencies = instances
+
+                    if (instance.alt != null) {
+                        val instances2 by find(Model1) { "name" by instance.alt }
+
+                        wait()
+
+                        instance.dependencies = instances2
+                    }
+                }
+            }
+        }
+
+        monkt += Model1
+        monkt += Model2
+
+        runBlocking {
+            monkt.init()
+
+            Model1.getCollection().insertManySuspend(
+                { "name" by "A"; "value" by "0" },
+                { "name" by "A"; "value" by "1" },
+                { "name" by "B"; "value" by "2" },
+                { "name" by "B"; "value" by "3" },
+                { "name" by "B"; "value" by "4" },
+                { "name" by "C"; "value" by "5" },
+                { "name" by "C"; "value" by "6" },
+                { "name" by "C"; "value" by "7" },
+                { "name" by "C"; "value" by "8" },
+            )
+
+            val instances = Model2.create(
+                { "name" by "A"; "alt" by "C" },
+                { "name" by "B" }
+            )
+
+            assertEquals(listOf("5", "6", "7", "8"), instances[0].dependencies.map { it.value })
+            assertEquals(listOf("2", "3", "4"), instances[1].dependencies.map { it.value })
         }
     }
 }
