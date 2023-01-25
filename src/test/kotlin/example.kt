8cf19ace3b3618279c723b5fe6468d171d405def
Sulaiman Oboody <lsafer@cufy.org>
2023-01-25 17:44:17 +0300

    test: bulk aggregation test

--- src/test/kotlin/example.kt
+++ src/test/kotlin/example.kt
@@ -1,7 +1,7 @@
 package org.cufy.monkt.test
 
 import kotlinx.coroutines.runBlocking
-import org.cufy.bson.bstring
+import org.cufy.bson.*
 import org.cufy.monkt.*
 import org.cufy.monkt.schema.*
 import org.cufy.monkt.schema.extension.*
@@ -276,6 +276,70 @@ class ExampleTest {
 
             val document2 = Model1()
             assertFails { document2.save() }
+        }
+    }
+
+    @Test
+    fun `bulkAggregation offset and limits`() {
+        // @formatter:off
+        data class Document1(var value1: String = "") : Document
+        data class Document2(var value2: String = "") : Document
+        data class Document3(var value3: String = "") : Document
+        data class Document4(var value4: String = "") : Document
+        data class Document5(var value5: String = "") : Document
+
+        val Model1 = Model("Document1", ::Document1) { field(Document1::value1, StringSchema) }
+        val Model2 = Model("Document2", ::Document2) { field(Document2::value2, StringSchema) }
+        val Model3 = Model("Document3", ::Document3) { field(Document3::value3, StringSchema) }
+        val Model4 = Model("Document4", ::Document4) { field(Document4::value4, StringSchema) }
+        val Model5 = Model("Document5", ::Document5) { field(Document5::value5, StringSchema) }
+        // @formatter:on
+
+        val models = listOf(Model1, Model2, Model3, Model4, Model5)
+
+        runBlocking {
+            models.forEach { monkt += it }
+            monkt.init()
+
+            for (i in 10..50) {
+                models.forEachIndexed { index, model ->
+                    model.create({ "value${index + 1}" by "$i" })
+                }
+            }
+
+            val documents = models.aggregate(
+                List(models.size) { index ->
+                    {
+                        by {
+                            `$match` by {
+                                "value${index + 1}" by {
+                                    `$regex` by bregex("$index$", "gi")
+                                }
+                            }
+                        }
+                    }
+                },
+                {
+                    by { `$sort` by { "_id" by bint32(1) } }
+                    by { `$skip` by bint32(5) }
+                    by { `$limit` by bint32(15) }
+                },
+            )
+
+            assertEquals(listOf(
+                "20" to Model1, "21" to Model2, "22" to Model3, "23" to Model4, "24" to Model5,
+                "30" to Model1, "31" to Model2, "32" to Model3, "33" to Model4, "34" to Model5,
+                "40" to Model1, "41" to Model2, "42" to Model3, "43" to Model4, "44" to Model5
+            ), documents.map { document ->
+                when (document) {
+                    is Document1 -> document.value1 to Model1
+                    is Document2 -> document.value2 to Model2
+                    is Document3 -> document.value3 to Model3
+                    is Document4 -> document.value4 to Model4
+                    is Document5 -> document.value5 to Model5
+                    else -> error("")
+                }
+            })
         }
     }
 }
